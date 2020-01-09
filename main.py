c91fcc8f171238064f71e4435dcbb9a7e717eb45
Tanmay Vijay <51825251+tanmayvijay@users.noreply.github.com>
2020-01-09 09:05:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import filedialog, messagebox
 import imageio
 import os
 
@@ -7,7 +8,7 @@ print("\n\nClip:", clip, "\n\n")
 
 def convert_to_gif(video_path, target_format):
     gif_path = os.path.splitext(video_path)[0] + target_format
-    print(f'Converting: {input_path} \n to \n {output_path}')
+    print(f'Converting: {video_path} \n to \n {gif_path}')
     reader = imageio.get_reader(video_path)
     fps = reader.get_meta_data()['fps']
 
